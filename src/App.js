57baec3632201ddd7126ae9f913cc590e2bfb303
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-12-17 23:10:33 -0600

    Header included with new Favicon.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar/NavBar';
+import Header from './components/Header/Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <React.Fragment>
         <NavBar/>
-        Home Something
-      </div>
+        <Header/>
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.scss'
+
+export default function Header() {
+  return (
+    <div className="header-container">
+      <h1> designer &amp; visual storyteller based in utah.</h1>
+    </div>
+  )
+}
