ad5395f0ed01b440fc74168a42ab2ba933080797
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-12-17 23:32:00 -0600

    Added Portfolio and Footer section.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar/NavBar';
 import Header from './components/Header/Header';
+import PortfolioPreview from './components/PortfolioPreview/PortfolioPreview';
+import Footer from './components/Footer/Footer'
 
 class App extends Component {
   render() {
@@ -8,6 +10,8 @@ class App extends Component {
       <React.Fragment>
         <NavBar/>
         <Header/>
+        <PortfolioPreview/>
+        <Footer/>
       </React.Fragment>
     );
   }
---
+++ src/components/PortfolioPreview/PortfolioPreview.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './PortfolioPreview.scss'
+
+export default class PortfolioPreview extends Component {
+  render() {
+    return (
+      <div className="port-preview-container">
+        Portfolio
+      </div>
+    )
+  }
+}
