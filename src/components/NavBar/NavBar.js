26febfc92a6372a63bdacc0f4260ed0f413a12f0
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-12-17 22:35:26 -0600

    Basic NavBar set up.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import './NavBar.scss'
+import logo from '../../assets/logo.png'
 
 export default function Navbar() {
   return (
     <nav>
+
       <div className="panel-one">
-      one
+      <img src={logo} alt="logo"/>
       </div>
 
       <div className="panel-two">
-      two
+      <p> This will be information about the designer, where the designer lives, and what makes the designer unique to her discipline. </p>
       </div>
 
       <div className="panel-three">
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-      <li>Dribble</li>
-      <li>LinkedIn</li>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Dribble</li>
+          <li>LinkedIn</li>
+        </ul>
       </div>
+
     </nav>
   )
 }
