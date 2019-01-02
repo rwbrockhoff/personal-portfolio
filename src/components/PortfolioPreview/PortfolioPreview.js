e730a373b817108038ab44d1d0a8fe60bc31ba78
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-01-01 22:48:56 -0600

    Working on mobile-responsive. New local fonts included.

--- src/components/PortfolioPreview/PortfolioPreview.js
+++ src/components/PortfolioPreview/PortfolioPreview.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
 import './PortfolioPreview.scss'
+
+//----Images----//
 import LookingFox from '../../assets/port_images/LookingFox.png'
 import IndigoFern from '../../assets/port_images/IndigoFern.png'
 import DouglasMartin from '../../assets/port_images/DouglasMartin.png'
+import Scharr from '../../assets/port_images/Scharr.png'
+import MoonValley from '../../assets/port_images/MoonValley.png'
+import Guzman from '../../assets/port_images/Guzman.png'
+//----Images----//
+
 export default class PortfolioPreview extends Component {
   render() {
     return (
@@ -18,6 +25,20 @@ export default class PortfolioPreview extends Component {
 
         <div className="port-row-split">
           <img src={DouglasMartin} alt="Looking Fox Logo"/>
+        </div>
+      </div>
+
+      <div className="port-row">
+        <img src={Scharr} alt="Looking Fox Logo"/>
+      </div>
+
+      <div className="split-container">
+        <div className="port-row-split">
+          <img src={MoonValley} alt="Looking Fox Logo"/>
+        </div>
+
+        <div className="port-row-split">
+          <img src={Guzman} alt="Looking Fox Logo"/>
         </div>
       </div>
 
