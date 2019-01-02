55552c335583a1eff1c3b361ce1e2207ea1f7b8b
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-01-02 16:46:11 -0600

    Investment is responsive and layout is in place.

--- src/components/Investment/Investment.js
+++ src/components/Investment/Investment.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import NavBar from '../NavBar/NavBar'
+import './Investment.scss'
 
 export default function Investment() {
   return (
-    <div>
+    <React.Fragment>
       <NavBar/>
-      Investment
-    </div>
+      <div className="invest-container">
+      <h1>the process</h1>
+      <h4>from start to finish.</h4>
+
+        <div className="step-container">
+          <div className="step">
+          one
+          </div>
+          <div className="step">
+          two
+          </div>
+          <div className="step">
+          three
+          </div>
+        </div>
+
+      </div>
+    </React.Fragment>
   )
 }
