80ca691d84e31c7ca6331667b5be3c95df78857f
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-01-02 22:12:44 -0600

    Images updated. Noise removed for portfolio images.

--- src/components/PortfolioPreview/PortfolioPreview.js
+++ src/components/PortfolioPreview/PortfolioPreview.js
@@ -8,6 +8,9 @@ import DouglasMartin from '../../assets/port_images/DouglasMartin.png'
 import Scharr from '../../assets/port_images/Scharr.png'
 import MoonValley from '../../assets/port_images/MoonValley.png'
 import Guzman from '../../assets/port_images/Guzman.png'
+import Brooks from '../../assets/port_images/Brooks.png'
+import Oyl from '../../assets/port_images/Oyl.png'
+import Hodson from '../../assets/port_images/Hodson.png'
 //----Images----//
 
 export default class PortfolioPreview extends Component {
@@ -40,7 +43,22 @@ export default class PortfolioPreview extends Component {
         <div className="port-row-split">
           <img src={Guzman} alt="Looking Fox Logo"/>
         </div>
+      </div> 
+
+
+      <div className="port-row">
+        <img src={Brooks} alt="Looking Fox Logo"/>
       </div>
+
+      <div className="split-container">
+        <div className="port-row-split">
+          <img src={Oyl} alt="Looking Fox Logo"/>
+        </div>
+
+        <div className="port-row-split">
+          <img src={Hodson} alt="Looking Fox Logo"/>
+        </div>
+      </div> 
 
       </React.Fragment>
     )
