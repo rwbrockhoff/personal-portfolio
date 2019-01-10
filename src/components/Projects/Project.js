ea83871ac714699dcbaa3c9359e89bdd75dcf0f1
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-01-10 15:30:10 -0600

    Buttons added with live links.

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -8,6 +8,13 @@ export default function Project(props) {
             <div className="tab">
                 <h2>{props.title}</h2>
             </div>
+            <div className="button-container">
+                <a href={props.copy.live} ><button><i className="fas fa-bolt" />live site</button></a>
+                <a href={props.copy.github}>
+                    <button>
+                        <i className="fab fa-github" />github
+                </button></a>
+            </div>
             <div className="text-container">
                 <p>{props.copy.copy}</p>
                 <ul>
@@ -24,16 +31,6 @@ export default function Project(props) {
                     <img src={props.image} className={deviceStyle} alt="Animated project preview" />
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
-
-
-
-
-{/* <div className="project-container">
-                <div className="laptop-view">
-                    <img src={props.device} className="mac-image" alt="Mac Laptop" />
-                    <img src={props.image} className="laptop-project-image" />
-                </div>
-            </div> */}
--- src/components/Projects/copyText.js
+++ src/components/Projects/copyText.js
@@ -2,25 +2,35 @@ export const dulyText = {
     copy: 'A full-stack note-taking app created to solidify proficiency in React, while delving into extensive libraries such as Stripe (Elements, Subscription), and Medium-Draft (Draft.js).',
     bullets: ["Received 1st Place, Best Overall Award",
         "Design is customizable for each user's preferences",
-        "Uses a custom pomodoro to keep users focused on tasks"]
+        "Uses a custom pomodoro to keep users focused on tasks"],
+    live: "https://www.dulynotes.com/#/",
+    github: "https://rwbrockhoff.github.io/note/"
 }
 
 export const orangesText = {
     copy: 'A full-stack, responsive game based on Apples to Apples. Built mobile-first in React, using socket.io as the bulk of the technology to communicate game information.',
-    bullets: ["Utilizes socket.io for web-socket communication", "Focus on animation and game-like immersion", "Emphasis on responsive design"]
+    bullets: ["Utilizes socket.io for web-socket communication", "Focus on animation and game-like immersion", "Emphasis on responsive design"],
+    live: "https://oranges2oranges2.firebaseapp.com/#/",
+    github: "https://rwbrockhoff.github.io/oranges/"
 }
 
 export const aktText = {
     copy: 'A full-stack todo list app built with Vue to demonstrate enthusiam towards new technology. Akt is built as a clone of Wunderlist. Akt uses Vuex, Vue Routing, Bootstrap, Auth0, and Postgres.',
-    bullets: ["Uses Vuex for state management", "Design incorporates Bootstrap components", "Subtle keyframe animations and styling"]
+    bullets: ["Uses Vuex for state management", "Design incorporates Bootstrap components", "Subtle keyframe animations and styling"],
+    live: "https://aktlist.com/#/",
+    github: "https://rwbrockhoff.github.io/tovue/"
 }
 
 export const blokText = {
     copy: 'React Native application for the pomodoro technique, using a custom built timer, React Native elements, and Animatable for animations. All graphics designed in Illustrator for app launch on the Play Store.',
-    bullets: ["Released React Native application", "Emphasis on animation and native elements", "Develop understanding of Gradle and build releases"]
+    bullets: ["Released React Native application", "Emphasis on animation and native elements", "Develop understanding of Gradle and build releases"],
+    live: "https://play.google.com/store/apps/details?id=com.blok",
+    github: "https://github.com/rwbrockhoff/blok"
 }
 
 export const lfText = {
     copy: 'CMS web application built in React for professional photographers. Focuses on reusable components, using many tables and joins in Postgres, and incorporating Material UI and Bootstrap for a clean design.',
-    bullets: ["Focuses on reusable components", "Uses complex table relations in Postgres", "Incorporates Material UI and Bootstrap"]
+    bullets: ["Focuses on reusable components", "Uses complex table relations in Postgres", "Incorporates Material UI and Bootstrap"],
+    live: "https://www.lookingfox.com/#/",
+    github: "https://rwbrockhoff.github.io/ecrm/"
 }
