57170a950b083e55d7550b97bfdc2e83fdea23c4
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-06-29 00:06:27 -0600

    feat: new nav copy.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,68 +1,139 @@
-import React, { Component } from 'react'
-import './NavBar.scss'
-import { Link } from 'react-router-dom'
-import { withRouter } from 'react-router-dom';
-import logo from '../../assets/logo.png'
-
+import React, { Component } from "react";
+import "./NavBar.scss";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import logo from "../../assets/logo.png";
 
 class Navbar extends Component {
   constructor(props) {
-    super(props)
-    this.state = { menuOpen: false }
+    super(props);
+    this.state = { menuOpen: false };
   }
 
   toggleMenu = () => {
-    this.setState({ menuOpen: !this.state.menuOpen })
-  }
+    this.setState({ menuOpen: !this.state.menuOpen });
+  };
 
   render() {
-    const { menuOpen } = this.state
-    const iconStyles = menuOpen ? 'fas fa-times side-menu-icon' : 'fas fa-bars side-menu-icon'
-    const isHomePage = this.props.history.location.pathname === "/" ? '' : '0ms'
+    const { menuOpen } = this.state;
+    const iconStyles = menuOpen
+      ? "fas fa-times side-menu-icon"
+      : "fas fa-bars side-menu-icon";
+    const isHomePage =
+      this.props.history.location.pathname === "/" ? "" : "0ms";
 
     return (
-      <nav className="main-nav" style={{ animationDuration: isHomePage, animationDelay: isHomePage }}>
-
+      <nav
+        className="main-nav"
+        style={{ animationDuration: isHomePage, animationDelay: isHomePage }}
+      >
         <div className="panel-one">
-          <Link to="/"><img src={logo} alt="logo" ref={re => this.logoRef = re} /></Link>
+          <Link to="/">
+            <img src={logo} alt="logo" ref={re => (this.logoRef = re)} />
+          </Link>
         </div>
 
         <div className="panel-two">
-          <p> Hi, I'm Ryan. I'm a web developer and designer. When I'm not coding, you'll find me hiking, reading, or telling puns.</p>
-          <div className="line" style={{ animationDuration: isHomePage, animationDelay: isHomePage }} />
+          <p>
+            {" "}
+            Hi, I'm Ryan. I'm a web developer and user experience designer. When
+            I'm not coding or working on a design, you'll find me hiking,
+            reading, or telling puns.
+          </p>
+          <div
+            className="line"
+            style={{
+              animationDuration: isHomePage,
+              animationDelay: isHomePage
+            }}
+          />
         </div>
 
         <div className="panel-three">
           <ul>
+            <Link to="/contact">
+              {" "}
+              <li>
+                <i className="far fa-envelope" />
+                Contact
+              </li>{" "}
+            </Link>
+            <a
+              href="https://linkedin.com/in/ryanbrockhoff"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <li>
+                <i className="fab fa-linkedin" /> LinkedIn
+              </li>
+            </a>
 
-            <Link to="/contact"> <li><i className="far fa-envelope" />Contact</li> </Link>
-            <a href="https://linkedin.com/in/ryanbrockhoff" target="_blank" rel="noopener noreferrer"><li><i className="fab fa-linkedin" />  LinkedIn</li></a>
+            <a
+              href="https://github.com/rwbrockhoff"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <li>
+                <i className="fab fa-github" /> Github
+              </li>
+            </a>
 
-            <a href="https://github.com/rwbrockhoff" target="_blank" rel="noopener noreferrer"><li><i className="fab fa-github" />  Github</li></a>
-
-            <a href="https://medium.com/@brockhoff" target="_blank" rel="noopener noreferrer"><li><i className="fab fa-medium-m" />  Medium</li></a>
+            <a
+              href="https://medium.com/@brockhoff"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <li>
+                <i className="fab fa-medium-m" /> Medium
+              </li>
+            </a>
           </ul>
         </div>
 
-        <i className={iconStyles}
-          onClick={this.toggleMenu} />
+        <i className={iconStyles} onClick={this.toggleMenu} />
 
-
-        {menuOpen ?
+        {menuOpen ? (
           <nav className="side-menu">
-            <Link to="/contact"> <li><i className="far fa-envelope" />Contact</li> </Link>
-            <a href="https://linkedin.com/in/ryanbrockhoff" target="_blank" rel="noopener noreferrer"><li><i className="fab fa-linkedin" />  LinkedIn</li></a>
+            <Link to="/contact">
+              {" "}
+              <li>
+                <i className="far fa-envelope" />
+                Contact
+              </li>{" "}
+            </Link>
+            <a
+              href="https://linkedin.com/in/ryanbrockhoff"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <li>
+                <i className="fab fa-linkedin" /> LinkedIn
+              </li>
+            </a>
 
-            <a href="https://github.com/rwbrockhoff" target="_blank" rel="noopener noreferrer"><li><i className="fab fa-github" />  Github</li></a>
+            <a
+              href="https://github.com/rwbrockhoff"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <li>
+                <i className="fab fa-github" /> Github
+              </li>
+            </a>
 
-            <a href="https://medium.com/@brockhoff" target="_blank" rel="noopener noreferrer"><li><i className="fab fa-medium-m" />  Medium</li></a>
+            <a
+              href="https://medium.com/@brockhoff"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <li>
+                <i className="fab fa-medium-m" /> Medium
+              </li>
+            </a>
           </nav>
-          :
-          null
-        }
-
+        ) : null}
       </nav>
-    )
+    );
   }
 }
 export default withRouter(Navbar);
