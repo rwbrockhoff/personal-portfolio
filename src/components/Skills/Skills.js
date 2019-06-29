38b51eeba3187d65c95045a847b6b6eff7d00369
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-06-28 23:47:56 -0600

    feat: icons updated to reflect current skillset.

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,60 +1,65 @@
-import React from 'react'
-import './Skills.scss'
+import React from "react";
+import "./Skills.scss";
 
-import HTML from '../../assets/icons/html.png'
-import CSS from '../../assets/icons/css.png'
-import SASS from '../../assets/icons/sass.png'
-import JS from '../../assets/icons/js.jpg'
-import R from '../../assets/icons/react.png'
-import Vue from '../../assets/icons/vue.png'
-import Node from '../../assets/icons/node.png'
-import SQL from '../../assets/icons/postgres.png'
-import Jest from '../../assets/icons/jest.png'
-import Adobe from '../../assets/icons/adobe.png'
+import HTML from "../../assets/icons/html.png";
+import CSS from "../../assets/icons/css.png";
+import SASS from "../../assets/icons/sass.png";
+import JS from "../../assets/icons/js.jpg";
+import R from "../../assets/icons/react.png";
+import TS from "../../assets/icons/typescript.png";
+import Node from "../../assets/icons/node.png";
+import SQL from "../../assets/icons/postgres.png";
+import Jest from "../../assets/icons/jest.png";
+import Sketch from "../../assets/icons/sketch.png";
+import Invision from "../../assets/icons/invision.png";
 
 export default function Skills() {
-    return (
-        <section className="skills-section">
-            <div className="skill large-icon">
-                <img src={R} alt="React Icon" />
-                <p>React</p>
-            </div>
-            <div className="skill larger-icon">
-                <img src={Node} alt="Node Icon" />
-                <p>Node</p>
-            </div>
-            <div className="skill">
-                <img src={JS} alt="Javascript Icon" />
-                <p>Javascript</p>
-            </div>
-            <div className="skill">
-                <img src={Vue} alt="Vue Icon" />
-                <p>Vue</p>
-            </div>
-            <div className="skill">
-                <img src={SQL} alt="Postgresql Icon" />
-                <p>Postgres</p>
-            </div>
-            <div className="skill">
-                <img src={Jest} alt="Jest Icon" />
-                <p>Jest</p>
-            </div>
-            <div className="skill large-icon">
-                <img src={SASS} alt="Sass Icon" />
-                <p>Sass</p>
-            </div>
-            <div className="skill">
-                <img src={HTML} alt="HTML Icon" />
-                <p>HTML</p>
-            </div>
-            <div className="skill">
-                <img src={CSS} alt="CSS Icon" />
-                <p>CSS</p>
-            </div>
-            <div className="skill">
-                <img src={Adobe} alt="Adobe Icon" />
-                <p>Adobe</p>
-            </div>
-        </section>
-    )
+  return (
+    <section className="skills-section">
+      <div className="skill large-icon">
+        <img src={R} alt="React Icon" />
+        <p>React</p>
+      </div>
+      <div className="skill larger-icon">
+        <img src={Node} alt="Node Icon" />
+        <p>Node</p>
+      </div>
+      <div className="skill">
+        <img src={JS} alt="Javascript Icon" />
+        <p>Javascript</p>
+      </div>
+      <div className="skill">
+        <img src={TS} alt="Typescript Icon" />
+        <p>Typescript</p>
+      </div>
+      <div className="skill">
+        <img src={SQL} alt="Postgresql Icon" />
+        <p>Postgresql</p>
+      </div>
+      <div className="skill">
+        <img src={Jest} alt="Jest Icon" />
+        <p>Jest</p>
+      </div>
+      <div className="skill large-icon">
+        <img src={SASS} alt="Sass Icon" />
+        <p>Sass</p>
+      </div>
+      <div className="skill">
+        <img src={Sketch} alt="Sketch Icon" />
+        <p>Sketch</p>
+      </div>
+      <div className="skill">
+        <img src={Invision} alt="Invision Icon" />
+        <p>Invision</p>
+      </div>
+      <div className="skill">
+        <img src={HTML} alt="HTML Icon" />
+        <p>HTML</p>
+      </div>
+      <div className="skill">
+        <img src={CSS} alt="CSS Icon" />
+        <p>CSS</p>
+      </div>
+    </section>
+  );
 }
