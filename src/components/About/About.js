331d2c4b5a122bce62f932a951cb6f0e29cdb7ae
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-06-28 23:51:24 -0600

    feat: update about copy.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,20 +1,31 @@
-import React, { Component } from 'react'
-import './About.scss'
-import Profile from '../../assets/profile.png'
+import React, { Component } from "react";
+import "./About.scss";
+import Profile from "../../assets/profile.png";
 
 export default class About extends Component {
   render() {
     return (
       <section className="about-section">
         <img src={Profile} className="profile-picture" alt="portrait" />
-        <p>I'm a web developer with an emphasis on frontend and design. Previously, I was an elopement photographer + videographer with my fiance. When I wasn't standing on cliff edges in Yosemite or Moab, I was passionately coding and learning new things. I'm constantly challenging myself to improve as a developer and a designer. I'm at my happiest when I'm building useful and intuitive software.</p>
-        <a href="https://github.com/rwbrockhoff/personal-portfolio" target="_blank" rel="noopener noreferrer">
+        <p>
+          I'm a web developer and user experience designer. I currently work as
+          a remote software engineer for HyperCarrot, where I develop across a
+          full stack, and create various designs and prototypes. I'm constantly
+          challenging myself to improve as a developer and a designer. I'm at my
+          happiest when I can work closely with logic and design on a daily
+          basis. Currently open to freelance opportunities.
+        </p>
+        <a
+          href="https://github.com/rwbrockhoff/personal-portfolio"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <p className="from-scratch">
             <i className="fab fa-github" /> I built this portfolio using React.
             <div className="hidden-line" />
           </p>
         </a>
       </section>
-    )
+    );
   }
 }
