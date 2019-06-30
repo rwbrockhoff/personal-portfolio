8d5744f6fee21d425c93a32521f79e584aefa391
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-06-29 22:07:52 -0600

    feat: remove akt project for future BOS project.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,50 +1,46 @@
-import React from 'react'
-import './Projects.scss'
-import Project from './Project'
-import { dulyText, aktText, lfText, orangesText, blokText } from './copyText'
-import dulyScreen from "../../assets/project-graphics/duly_screen.gif"
-import aktScreen from '../../assets/project-graphics/akt.gif'
-import blokScreen from '../../assets/project-graphics/blok.gif'
-import mac from '../../assets/project-graphics/mac.png'
-import iphone from '../../assets/project-graphics/iphone.png'
-import orangesScreen from '../../assets/project-graphics/orangesdemo.gif'
-import lfScreen from '../../assets/project-graphics/lookingFoxScreen.gif'
+import React from "react";
+import "./Projects.scss";
+import Project from "./Project";
+import { dulyText, lfText, orangesText, blokText } from "./copyText";
+import dulyScreen from "../../assets/project-graphics/duly_screen.gif";
+import blokScreen from "../../assets/project-graphics/blok.gif";
+import mac from "../../assets/project-graphics/mac.png";
+import iphone from "../../assets/project-graphics/iphone.png";
+import orangesScreen from "../../assets/project-graphics/orangesdemo.gif";
+import lfScreen from "../../assets/project-graphics/lookingFoxScreen.gif";
 
 export default function Projects() {
-    return (
-        <section className="projects-section">
-            <Project
-                image={lfScreen}
-                device={mac}
-                title="looking fox"
-                copy={lfText} />
+  return (
+    <section className="projects-section">
+      <Project
+        image={lfScreen}
+        device={mac}
+        title="looking fox"
+        copy={lfText}
+      />
 
-            <Project
-                mobile={true}
-                image={orangesScreen}
-                device={iphone}
-                title="oranges"
-                copy={orangesText} />
+      <Project
+        mobile={true}
+        image={orangesScreen}
+        device={iphone}
+        title="oranges"
+        copy={orangesText}
+      />
 
-            <Project
-                image={dulyScreen}
-                device={mac}
-                title="duly notes"
-                copy={dulyText} />
+      <Project
+        image={dulyScreen}
+        device={mac}
+        title="duly notes"
+        copy={dulyText}
+      />
 
-            <Project
-                mobile={true}
-                image={blokScreen}
-                device={iphone}
-                title="blok"
-                copy={blokText} />
-
-            <Project
-                image={aktScreen}
-                device={mac}
-                title="akt"
-                copy={aktText} />
-
-        </section>
-    )
+      <Project
+        mobile={true}
+        image={blokScreen}
+        device={iphone}
+        title="blok"
+        copy={blokText}
+      />
+    </section>
+  );
 }
--- src/components/Projects/copyText.js
+++ src/components/Projects/copyText.js
@@ -1,36 +1,47 @@
 export const dulyText = {
-    copy: 'A full-stack note-taking app created to solidify proficiency in React, while delving into extensive libraries such as Stripe (Elements, Subscription), and Medium-Draft (Draft.js).',
-    bullets: ["Received 1st Place, Best Overall Award",
-        "Design is customizable for each user's preferences",
-        "Uses a custom pomodoro to keep users focused on tasks"],
-    live: "https://www.dulynotes.com/#/",
-    github: "https://rwbrockhoff.github.io/note/"
-}
+  copy:
+    "A full-stack note-taking app created to solidify proficiency in React, while delving into extensive libraries such as Stripe (Elements, Subscription), and Medium-Draft (Draft.js).",
+  bullets: [
+    "Received 1st Place, Best Overall Award",
+    "Design is customizable for each user's preferences",
+    "Uses a custom pomodoro to keep users focused on tasks"
+  ],
+  live: "https://www.dulynotes.com/#/",
+  github: "https://rwbrockhoff.github.io/note/"
+};
 
 export const orangesText = {
-    copy: 'A full-stack, responsive game based on Apples to Apples. Built mobile-first in React, using socket.io as the bulk of the technology to communicate game information.',
-    bullets: ["Responsible for the majority of frontend implementation", "Developed prototypes to improve user-flow using Adobe XD", "Debugged and pair-programmed with team through various challenges"],
-    live: "https://oranges2oranges2.firebaseapp.com/#/",
-    github: "https://rwbrockhoff.github.io/oranges/"
-}
-
-export const aktText = {
-    copy: 'A full-stack todo list app built with Vue to demonstrate enthusiam towards new technology. Akt is built as a clone of Wunderlist. Akt uses Vuex, Vue Routing, Bootstrap, Auth0, and Postgres.',
-    bullets: ["Uses Vuex for state management", "Design incorporates Bootstrap components", "Subtle keyframe animations and styling"],
-    live: "https://aktlist.com/#/",
-    github: "https://rwbrockhoff.github.io/tovue/"
-}
+  copy:
+    "A full-stack, responsive game based on Apples to Apples. Built mobile-first in React, using socket.io as the bulk of the technology to communicate game information.",
+  bullets: [
+    "Responsible for the majority of frontend implementation",
+    "Developed prototypes to improve user-flow using Adobe XD",
+    "Debugged and pair-programmed with team through various challenges"
+  ],
+  live: "https://oranges2oranges2.firebaseapp.com/#/",
+  github: "https://rwbrockhoff.github.io/oranges/"
+};
 
 export const blokText = {
-    copy: 'React Native application for the pomodoro technique, using a custom built timer, React Native elements, and Animatable for animations. All graphics designed in Illustrator for app launch on the Play Store.',
-    bullets: ["Released React Native application", "Emphasis on animation and native elements", "Develop understanding of Gradle and build releases"],
-    live: "https://play.google.com/store/apps/details?id=com.blok",
-    github: "https://github.com/rwbrockhoff/blok"
-}
+  copy:
+    "React Native application for the pomodoro technique, using a custom built timer, React Native elements, and Animatable for animations. All graphics designed in Illustrator for app launch on the Play Store.",
+  bullets: [
+    "Released React Native application",
+    "Emphasis on animation and native elements",
+    "Develop understanding of Gradle and build releases"
+  ],
+  live: "https://play.google.com/store/apps/details?id=com.blok",
+  github: "https://github.com/rwbrockhoff/blok"
+};
 
 export const lfText = {
-    copy: 'CMS web application built in React for professional photographers. Focuses on reusable components, using many tables and joins in Postgres, and incorporating Material UI and Bootstrap for a clean design.',
-    bullets: ["Focuses on reusable components", "Uses complex table relations in Postgres", "Incorporates Material UI and Bootstrap"],
-    live: "https://www.lookingfox.com/#/",
-    github: "https://rwbrockhoff.github.io/ecrm/"
-}
+  copy:
+    "CMS web application built in React for professional photographers. Focuses on reusable components, using many tables and joins in Postgres, and incorporating Material UI and Bootstrap for a clean design.",
+  bullets: [
+    "Focuses on reusable components",
+    "Uses complex table relations in Postgres",
+    "Incorporates Material UI and Bootstrap"
+  ],
+  live: "https://www.lookingfox.com/#/",
+  github: "https://rwbrockhoff.github.io/ecrm/"
+};
